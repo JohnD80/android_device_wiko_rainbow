ae40e05e013e5eacec9046e7a6996059e966c439
kirito96 <eagle1968@live.com>
2016-12-05 20:47:54 -0400

    support for making recoveries

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,11 +19,11 @@ DEVICE_RESOLUTION := 480x800
 BOARD_HAS_NO_SELECT_BUTTON := true
 
 # make_ext4fs requires numbers in dec format
-BOARD_BOOTIMAGE_PARTITION_SIZE := 5767168
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 6207168
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 943718400
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 968884224
-BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 6291456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 6291456
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 545259520
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1073741824
+BOARD_FLASH_BLOCK_SIZE := 512
 
 BOARD_KERNEL_CMDLINE :=
 BOARD_KERNEL_BASE := 0x10000000
@@ -52,18 +52,59 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KMODULES := true
 
 TARGET_PREBUILT_KERNEL := device/tct/yaris_m_gsm/kernel
-TARGET_RECOVERY_FSTAB := device/tct/yaris_m_gsm/rootdir/root/recovery.fstab
+TARGET_RECOVERY_FSTAB := device/tct/yaris_m_gsm/recovery/recovery.fstab
 
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := yaris_m_gsm,Yaris_M_GSM,4032A,Yaris_M,4033
 
-# Philz Recovery
+
+RECOVERY_VARIANT := twrp
 #RECOVERY_VARIANT := philz
-#BOARD_USE_MTK_LAYOUT := true
-#BOARD_USE_NTFS_3G := false
-#BOARD_HAS_NO_FB2PNG := true
-#NO_AROMA_FILE_MANAGER := true
+#RECOVERY_VARIANT := carliv
+
+ifneq ($(RECOVERY_VARIANT),carliv)
+TARGET_RECOVERY_FSTAB := device/TCT/Yaris_M_GSM/recovery/twrp.fstab
+endif
+
+
+# CARLIV
+ifeq ($(RECOVERY_VARIANT),carliv)
+BOARD_HAS_MTK := true
+BOARD_NEEDS_MTK_GETSIZE := true
+DEVICE_RESOLUTION := 480x800
+endif
+
+
+# PHILZ
+ifeq ($(RECOVERY_VARIANT),philz)
+TARGET_COMMON_NAME := TCT Yaris_M_GSM
+BOARD_USE_MTK_LAYOUT := true
+BOARD_MTK_BOOT_LABEL := "/bootimg"
+BOARD_USE_NTFS_3G := false
+BRIGHTNESS_SYS_FILE := "/sys/class/leds/lcd-backlight/brightness"
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+endif
+
+# TWRP
+ifeq ($(RECOVERY_VARIANT),twrp)
+TW_NO_EXFAT := true
+TWHAVE_SELINUX := false
+TW_THEME := portrait_mdpi
+TW_NO_EXFAT_FUSE := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file"
+TW_EXCLUDE_SUPERSU := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGB_565"
+DEVICE_RESOLUTION := 480x800
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
+endif
+
 
 # Deodex
 WITH_DEXPREOPT := false
